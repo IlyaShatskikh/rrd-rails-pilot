af2f72e34a6ce5c822fdea64bf97e21db65fb185
Ilya Shatskikh <ilya.shatskikh@ocrv.ru>
2020-05-13 16:18:27 +0300

    entity fix

--- src/main/java/rzd/rrd/rails/pilot/domain/DistancePassport.java
+++ src/main/java/rzd/rrd/rails/pilot/domain/DistancePassport.java
@@ -1,21 +1,50 @@
 package rzd.rrd.rails.pilot.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 
 import java.time.LocalDateTime;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE, onConstructor = @__(@PersistenceConstructor))
+@Data
 public class DistancePassport {
-    @Id @JsonIgnore
-    private final Long id;
-    private final Integer IDROW;
-    private final String SITEID;
-    private final String ASSETNUM;
-    private final Integer PUTGL_ID;
-    private final LocalDateTime CHANGEDA;
-    private final Integer ID_CATEG, ID_CLASS, ID_GRUP, M1, M2, KMN, KMK, PKN, PKK, MN, MK, L;
+    @Id
+    @JsonProperty("IDROW")
+    private final Integer idrow;
+    @JsonProperty("SITEID")
+    private final String siteid;
+    @JsonProperty("ASSETNUM")
+    private final String assetnum;
+    @JsonProperty("PUTGL_ID")
+    private final Integer putgl_id;
+    @JsonProperty("CHANGEDATE")
+    private final LocalDateTime changedate;
+    @JsonProperty("ID_CATEG")
+    private final Integer id_categ;
+    @JsonProperty("ID_CLASS")
+    private final Integer id_class;
+    @JsonProperty("ID_GRUP")
+    private final Integer id_grup;
+    @JsonProperty("M1")
+    private final Integer m1;
+    @JsonProperty("M2")
+    private final Integer m2;
+    @JsonProperty("KMN")
+    private final Integer kmn;
+    @JsonProperty("KMK")
+    private final Integer kmk;
+    @JsonProperty("PKN")
+    private final Integer pkn;
+    @JsonProperty("PKK")
+    private final Integer pkk;
+    @JsonProperty("MN")
+    private final Integer mn;
+    @JsonProperty("MK")
+    private final Integer mk;
+    @JsonProperty("L")
+    private final Integer l;
 }
