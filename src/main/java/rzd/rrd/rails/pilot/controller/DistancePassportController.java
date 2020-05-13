c6358fe2b11a37f90a55296f238feac62090320a
Ilya Shatskikh <ilya.shatskikh@ocrv.ru>
2020-05-13 11:20:46 +0300

    new rest endpoints

--- src/main/java/rzd/rrd/rails/pilot/controller/DistancePassportController.java
+++ src/main/java/rzd/rrd/rails/pilot/controller/DistancePassportController.java
@@ -3,6 +3,9 @@ package rzd.rrd.rails.pilot.controller;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,11 +21,17 @@ public class DistancePassportController {
     private final Logger log = LoggerFactory.getLogger(DistancePassportController.class);
     private final DistancePassportService service;
 
-    @GetMapping(value = "/dp") //produces = MediaType.APPLICATION_JSON_VALUE)
-    public String distancePassport(@RequestParam(name = "siteId") String siteId) throws Exception {
-        log.info("Request: distance passport. Arrgs: siteId {}", siteId);
+    @GetMapping(value = "/dp", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<DistancePassport>> getDistancePassports(@RequestParam(name = "siteId") String siteId) {
+        log.info("REQUEST: getDistancePassports. PARAM: siteId {}", siteId);
+        List<DistancePassport> passports = service.getDistancePassport(siteId);
+        return new ResponseEntity<>(passports, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/dp/send", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String sendDistancePassports(@RequestParam(name = "siteId") String siteId) {
+        log.info("REQUEST: sendDistancePassports. PARAM: siteId {}", siteId);
         List<DistancePassport> passports = service.getDistancePassport(siteId);
         return service.postPassportDistance(passports);
     }
-
 }
